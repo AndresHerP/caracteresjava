764778e2292632c0121e9bcebf365a86cc5e357b
AndresHerP <202343rd061@utez.edu.mx>
2025-05-06 12:56:33 -0600

    agrege los prin

--- OneDrive/Desktop/javaAndres/caracteres/Caracteres.java
+++ OneDrive/Desktop/javaAndres/caracteres/Caracteres.java
@@ -3,6 +3,9 @@ public class Caracteres {
         char letra = 'J';
         char simbolo = '@';
         char numeroChar = '9';
-
+        
+        System.out.println("letra =" + letra);
+        System.out.println("simbolo =" + simbolo);
+        System.out.println("numeroChar =" + Caracteres); 
     }
 }
